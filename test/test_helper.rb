2312cc422bd4b8bc65c6b566322aae08c5c8aa18
Chris Holtz <choltz@gmail.com>
2017-07-04 14:56:42 -0400

    Added simplecov test coverage gem dependency

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,8 @@
+require 'simplecov'
+SimpleCov.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'lightpipe'
-
 require 'minitest/autorun'
 require 'shoulda'
 require 'byebug'
