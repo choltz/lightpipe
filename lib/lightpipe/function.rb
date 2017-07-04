887997f2d6b929fe2b2ed9b872227c90e4035413
Chris Holtz <choltz@gmail.com>
2017-07-03 23:12:29 -0400

    Added some comments

--- lib/lightpipe/function.rb
+++ lib/lightpipe/function.rb
@@ -1,5 +1,13 @@
 module Lightpipe
   class Function < Proc
+    # Public: build a composition of functions.
+    #
+    # functions - either an array of functions or a parameter list of functions
+    #
+    # Notes: The functions provided can be any class that has a call method
+    #
+    # Returns an instance of this class whose call method invokes the chain
+    # of functions provided
     def self.compose(*functions)
       self.new do |args|
         [functions].flatten.reduce(args) do |result, function|
