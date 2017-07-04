f500f3ba864a309093c877a433623af1abeb5128
Chris Holtz <choltz@gmail.com>
2017-07-04 14:59:21 -0400

    Added a test case to handle a param list of functions

--- test/function_test.rb
+++ test/function_test.rb
@@ -30,5 +30,24 @@ class FunctionTest < Minitest::Test
 
       assert_equal "This is an example of text. It has formatting issues", composition.call(text)
     end
+
+    should "allow a param list of functions rather than an array of functions" do
+      text    = "This is an <b>example</b> of\n text. it has formatting issues."
+      remove_line_feeds = ->(text) { text.gsub(/\n+/, '') }
+      remove_markup     = ->(text) { text.gsub(/(<([^>]+)>)/, '') }
+      split_sentences   = ->(text) { text.split(/ *\. */) }
+      strip             = ->(text) { text.strip }
+      capitalize        = ->(text) { text.map(&:capitalize) }
+      join              = ->(array) { array.join('. ') }
+
+      composition = Function.compose  strip,
+                                      remove_line_feeds,
+                                      remove_markup,
+                                      split_sentences,
+                                      capitalize,
+                                      join
+
+      assert_equal "This is an example of text. It has formatting issues", composition.call(text)
+    end
   end
 end
